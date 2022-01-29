247d4b3fdee50afb50e4fb86271bed64ba061bf9
Max Makarov <mail@mrboss.ru>
2022-01-29 16:52:26 +0300

    Создание проекта

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Max on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
